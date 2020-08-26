3dc24a5a388b488cfb6a7a4586f65dd8fe44e877
huanghaoguo <2017211001000203@ecjtu.deu.cn>
2020-08-26 17:49:29 +0800

    项目优化

---
+++ src/main-dev.js
@@ -0,0 +1,65 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入treeTable依赖
+import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+// 导入进度条插件包对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request拦截器中，展示进度条 NProgress.start()
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须返回一个值
+  return config
+})
+// 在response拦截器中隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+Vue.component('tree-table', TreeTable)
+// 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+// 定义一个全局过滤器将时间毫秒化成日期格式
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+  // 年
+  const y = dt.getFullYear()
+  // 月
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  // 日
+  const d = (dt.getDate() + 1 + '').padStart(2, '0')
+  // 时
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  // 分
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  // 秒
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
